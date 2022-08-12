4731f2e77c856e063e41f844a986cd2eafc90aca
xatzos21 <xatzos21@gmail.com>
2022-08-12 09:41:15 +0200

    ex4

--- exercise.py
+++ exercise.py
@@ -1,7 +1,7 @@
 #Task1-ex4
 #Your task is to write a program which asks the user 
 # two times about two integer numbers to compare. 
- 
+
 count = 1
 while count <= 2:
 
@@ -18,7 +18,17 @@ while count <= 2:
 
     count += 1
 
+
     #Task2-ex4
     #Your task is to write a Python program to convert 
     # month name to a number of days. 
+    
 
+print("List of months: January, February, March, April, May, June, July, August, September, October, November, December")
+x = str(input("Input the name of Month:"))
+if x == str("January" or "March"or "May" or "July" or "August" or "October" or "December"):
+    print("Number of days: 31 days")
+elif x == str("April" or "June" or "September" or "November"):
+    print("Number of days: 30 days")
+else:
+    print("Number of days: 28 days")
